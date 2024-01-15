fe99b065769c4e6c01bf5cf173d6cb27e336e79f
MrProfessionalHacker <156511306+Congratulations62@users.noreply.github.com>
2024-01-16 00:56:47 +0300

    query order
    Signed-off-by: MrProfessionalHacker <156511306+Congratulations62@users.noreply.github.com>

---
+++ example/example.py
@@ -0,0 +1,4 @@
+TelebirrPrivateKey="TELEBIRR PUBLIC KEY"
+TelebirrAppKey="AIzaSyDEyj7Xmxl01jsbbb2-MBFj-W5hHRmAKyA"
+TelebirrAppID="1:551408497442:android:499cd272caec0d40ab2b7b"
+TelebirrShortCode="2005"
